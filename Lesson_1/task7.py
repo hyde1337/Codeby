0be9a5b7733c153ea9c33f3619c839e9a1a25c5f
Valera <isoloninka@gmail.com@>
2020-03-15 21:34:41 +0300

    For lessons

--- Lesson_1/task7.py
+++ Lesson_1/task7.py
@@ -1,3 +1,7 @@
-# a, b, c, d, e, f, g, h = 'fanat', 'apple', 'abracadabra', 'Tarantino', 'yahoo', 'yandex', 'gool', 'logo'
-
-print('apple' > 'fanat')
+print('Проверяем утверждение apple > fanat:', 'apple' > 'fanat')
+print('Проверяем утверждение abracadabra > Tarantino:', 'abracadabra' > 'Tarantino')
+print('Проверяем утверждение yahoo > yandex:', 'yahoo' > 'yandex')
+print('Проверяем утверждение gool > logo:', 'gool' > 'logo')
+print('Сформируем пары с наибольшими словами fanat > abracadabra:', 'fanat' > 'abracadabra')
+print('Сформируем пары с наибольшими словами yandex > logo:', 'yandex' > 'logo')
+print('Последняя пара состоит из самых больших слов fanat > yandex:', 'fanat' > 'yandex')
