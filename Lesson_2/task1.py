41b7102ba4789fc72ff931bdb7d10dbda71e36d9
Valera <isoloninka@gmail.com@>
2020-03-23 15:00:11 +0300

    For lessons

---
+++ Lesson_2/task1.py
@@ -0,0 +1,6 @@
+# Дано
+language = "Языки программирования: Php, Python, Goland, Java…"
+# Зададим переменную для изменения с прописных до заглавных
+language_for_ch = (language[23:200])
+# Выведем результат
+print((language[0:23]) + language_for_ch.upper())
---
+++ Lesson_2/task3.py
@@ -0,0 +1,6 @@
+# Дано
+word = 'Tsetnep'
+# Переменную для фикса
+fixed = word.lower()[::-1]
+# Вывод
+print(fixed)
