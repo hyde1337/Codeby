6d7f9d20260a6e188fcd4a6f269ea85e3a8ce3a5
Valera <isoloninka@gmail.com@>
2020-03-23 16:09:47 +0300

    For lessons

--- Lesson_2/task6.py
+++ Lesson_2/task6.py
@@ -1,9 +1,14 @@
+# Дано
 tool = 'Super-Puper MainTool /v2*'
+# Мультиплицируем звездочки
 stars = tool[24] * 14
-number = tool[21:24].replace('2', '1').replace('/', ' ')
+# Выделяем слово 'word'
 superWord = tool[0:5]
+# Выделяем слово 'tool'
 toolWord = tool[16:20]
-print(stars, superWord, toolWord, number, stars)
+# Меняем цифру 2 на цифру 1
+number = tool[22:24].replace('2', '1')
+# Выводим результат
 print(stars)
-print(superWord, toolWord, number)
+print(' ' + superWord + toolWord, number)
 print(stars)
