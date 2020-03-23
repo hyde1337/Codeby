d8e1ac116dc9de81e643cdb04f87c5e85a337950
Valera <isoloninka@gmail.com@>
2020-03-23 15:57:04 +0300

    For lessons

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,1 +1,13 @@
-history = 'История Python, одного из самых простых языков программирования, началась в 1989 году.'
+# Дано
+history = 'История Python, одного из самых простых языков программирования, началась в 1989 году.'  # "простор"
+# Выводим результаты разными способами
+print(history[32:37] + history[3:5])
+#####################################
+chHistory = history[32:39]
+print(chHistory.replace('ых', 'ор'))
+#####################################
+print(history[47:50] + history[1:5])
+#####################################
+fromPython = history[8].lower()
+fromLast = history[4] + history[3] + history[2] + history[1] + history[12] + fromPython + history[47]
+print(fromLast[::-1])
---
+++ Lesson_2/task6.py
@@ -0,0 +1,9 @@
+tool = 'Super-Puper MainTool /v2*'
+stars = tool[24] * 14
+number = tool[21:24].replace('2', '1').replace('/', ' ')
+superWord = tool[0:5]
+toolWord = tool[16:20]
+print(stars, superWord, toolWord, number, stars)
+print(stars)
+print(superWord, toolWord, number)
+print(stars)
