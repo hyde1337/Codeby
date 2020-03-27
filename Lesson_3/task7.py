02f4389415a1215f040173a4d5e95946048b346a
Valera <isoloninka@gmail.com@>
2020-03-28 01:57:31 +0300

    For lessons

--- Lesson_3/task7.py
+++ Lesson_3/task7.py
@@ -2,5 +2,12 @@
 slice_string = ['pgekjsgqlafrimzixwuiavukxdqifadmbdqvszcqizimkifcajycqijwuwwawmbbiiigevfrualbsgijbvskfskwczlbervxkmsgtxr'
                 'fxswmxsibffvaqrabgwxwcqzcrjiedsizjauufrfdiguzjxhcwlgjiduemddufkewasjfgavsrujgbisagzswdaeebwerdcluoqvgaj'
                 'abbelaadswzdebwgxvdfaugqjazlwvzejdgleszsrlqxnsrkbkqcvgwekwsqezll']
-x = slice_string.split()
-print(slice_string)
+# Переведем список с одним элементом в строку, затем сделаем из этой строки новый список по элементу и будем работать
+# с новым списком
+i = ''.join(slice_string)
+b = list(i)
+# На данном этапе почекав индексы командой c = b.index('*'), увидел, что каждая нужная буква занимает значение индекса
+# с шагом в 50, значит можно использовать один слайс и сразу сделать нужное слово, а затем просто перевести в строку
+g = b[::50]
+# Выводим результат в виде строки, используя метод join
+print(''.join(g))
