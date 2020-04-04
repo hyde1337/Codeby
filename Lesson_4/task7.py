cb4d40f48c0bd898fb38bb1814b74abd1b25434b
Valera <isoloninka@gmail.com@>
2020-04-04 23:27:58 +0300

    For lessons

--- Lesson_4/task7.py
+++ Lesson_4/task7.py
@@ -2,5 +2,7 @@ xyz = '1', '2', '3', '4'
 abc = ('первый', 'второй', 'третий', 'четвертый')
 
 finaDict = dict([(abc[0:1] + xyz[0:1])] + [(abc[1:2] + xyz[1:2])] + [(abc[2:3] + xyz[2:3])] + [(abc[3:4] + xyz[3:4])])
+finadict1 = {abc[0]: xyz[0], abc[1]: xyz[1], abc[2]: xyz[2], abc[3]: xyz[3]}
 
+print(tuple(finadict1.keys()))
 print(tuple(finaDict.keys()))
